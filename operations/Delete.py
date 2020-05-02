595cc1ed519c699b83dcdbd8bd309f6ef1476f96
tharaka <tharakasachintha27@gmail.com>
2020-05-02 15:49:52 +0530

    Update delete operation

--- operations/Delete.py
+++ operations/Delete.py
@@ -1,42 +1,29 @@
 import configparser
 import json
 
-class Add:
+class Delete:
     def __init__(self):
         pass
 
     @staticmethod
-    def addElemenet(table_name, data_dic):
+    def deleteElemenet(table_name, element_id):
         config = configparser.ConfigParser()
         config.read('database_data.ini')
         
-        lastID = 1
-        #let's find the last key of the table
-        for key in config[table_name]:
-            if lastID < int(key):
-                lastID = int(key)
-        print(lastID + 1)
-
-        print(str(json.dumps(data_dic)))
-        config[table_name][str(lastID + 1)] = json.dumps(data_dic)
+        config.remove_option(table_name, element_id)
         with open('database_data.ini', 'w') as f:
             config.write(f)
 
     @staticmethod
-    def addRelation(table_name, relation):
+    def deleteRelation(table_name, relation):
         config = configparser.ConfigParser()
         config.read('database_data.ini')
         
         current  = config[table_name]["eid"]
-        if current == "":
-            current = relation
-        else:
-            c = current
-            if len(current) == 1 and current == relation:
-                return
-            elif len(current) != 1 and relation in c.split(","):
-                return
-            current = current + "," + relation
+        current  = current.split(",")
+
+        current.remove(relation)
+        current = ','.join(current)
         #print(current)
         config[table_name]["eid"] = current
         with open('database_data.ini', 'w') as f:
@@ -49,7 +36,5 @@ class Add:
 #     Example Usage
 #
 #-------------------------------
-#data_dic = {"street": "Bulugahawatta Road5", "town": "Kadawatha5"}   
-#Add.addElemenet("Addresses",data_dic)
-Add.addRelation("Administrators", "7")
-
+#Delete.deleteElemenet("Addresses", "6")
+#Delete.deleteRelation("Managers", "6")
